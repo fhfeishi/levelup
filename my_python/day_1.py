728f7e29c8ec76188da66729a7a4b4605b79a059
baheas <fhfeishi@gmail.com>
2024-03-27 22:18:18 +0800

    first commit

---
+++ my_python/day_1.py
@@ -0,0 +1,39 @@
+# Day 1  基本的运算、变量类型
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(5 % 2)   # modulus(%)  # 1   # 取余
+print(5 // 2)  # Floor division operator(//) # 2  #取商
+# Checking data types
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
