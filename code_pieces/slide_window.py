d3772e2dfec98a94c7563e7ca2ad841e8b8fa7c5
feishi <fhfeishi@gmail.com>
2024-05-08 11:14:23 +0800

    update pil image slide-window

--- code_pieces/slide_window.py
+++ code_pieces/slide_window.py
@@ -111,10 +111,7 @@ class pil_img:
 
         return resized_image
 
-    # slide window for object detec
-    def paddedImageCrop_obj(self, image, crop_size=(640, 640), overlap=(0, 0), show_rows_cols_num=False, show_padding_wh=False,plot=False, all_box_show=False,
-                               coverred_box_show=False, show_onebyone=False, padded_img_show=None):
-
+    def paddedImageCrop_base(self, image, crop_size=(640, 640), overlap=(0, 0), draw_cropRectangle=False):
             width, height = image.size
             self.crop_width, self.crop_height = crop_size
 
@@ -152,119 +149,80 @@ class pil_img:
             padded_image = ImageOps.expand(image, border=(
                 0, 0, int(padding_width), int(padding_height)), fill=(0, 0, 0))
 
-            # 新建pillow画布 1------  全显示的分割框， 验证代码是不是写好
+            # 新建pillow画布 1------  全显示的分割框，     ---取出每个crop
             draw_img = padded_image.copy()
             draw_a = ImageDraw.Draw(draw_img)
             font = ImageFont.truetype("arial.ttf", 34)
-            # title
-            title = "overlap box a"
+            title = "crop-rectangle --a"
             title_font = ImageFont.truetype("arial.ttf", 40)
             title_width, title_height = draw_a.textsize(title, font=title_font)
             title_position = (int((width - title_width) / 2), 20)
             draw_a.text(title_position, title, font=title_font, fill=(255, 255, 255))
 
-            # 新建pillow画布 2------  有覆盖的
+            # 新建pillow画布 2------  每个crop依次写入画布2，     ---依次写入取出的每个crop
             canvas_height = self.crop_height + stride_height * num_rows
             canvas_width = self.crop_width + stride_width * num_cols
             canvas = Image.new("RGB", (canvas_width, canvas_height), (0, 0, 0))
             draw_b = ImageDraw.Draw(canvas)
-            # title
-            title = "overlap box b"
+            title = "crops write in onebyone  --b"
             title_font = ImageFont.truetype("arial.ttf", 40)
             title_width, title_height = draw_b.textsize(title, font=title_font)
             title_position = (int((width - title_width) / 2), 20)
             draw_b.text(title_position, title, font=title_font, fill=(255, 255, 255))
-
-            # print("---------------------")
-            # print("canvas_height:", canvas_height)
-            # print("canvas_width:", canvas_width)
-            # print("---------------------")
 
             # # 还是记住相对于在padded_image的位置，然后在这上面画预测框
             # 切割图像--所有的分割的预选框，有重叠overlap，
             image_blocks = []
             for r in range(num_rows + 1):
                 for c in range(num_cols + 1):
-
                     # 计算当前块的起始和结束位置
                     start_row = r * stride_height
                     end_row = start_row + self.crop_height
                     start_col = c * stride_width
                     end_col = start_col + self.crop_width
-
                     # 提取当前块
                     block = padded_image.crop((start_col, start_row, end_col, end_row))
+                    
+                    # may no use
                     if block.size[0] == self.crop_width and block.size[1] == self.crop_height:
-                        # 创建 ImageBlock 对象并存储
                         image_block = ImageBlock(block, r, c)
                         image_blocks.append(image_block)
                     else:
                         print(block.size)
                         continue
+                    
+                    if draw_cropRectangle:
+                        # 生成随机颜色
+                        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                        wid_a = random.randint(2, 21)
+                        
+                        # canvas-a
+                        # 绘制边界框
+                        draw_a.rectangle([(start_col, start_row), (end_col, end_row)], outline=color, width=wid_a)
+                        # 标记编号
+                        block_number = f"Block-bounding {r + 1}{c + 1}"
+                        draw_a.text((start_col + 10, start_row + 10), block_number, font=font, fill=color)
+                        
+                        # canvas-b
+                        canvas.paste(block, (start_col, start_row))
+                        # # # 绘制边界框 ---这里要用到end_row,end_col
+                        draw_b.rectangle([(start_col, start_row), (end_col, end_row)], outline=color, width=wid_a)
+                        # 标记编号
+                        block_number = f"Block {r + 1}{c + 1}"
+                        draw_b.text((start_col + 10, start_row + 10), block_number, font=font, fill=color)
 
-                    # 生成随机颜色
-                    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                        # # #crop一块一块往画布上写入，后写入的会覆盖之前的 
+                        # canvas.show()
+            
+            # ## 显示全部的crop-rectangle
+            draw_img.show()
 
-                    wid_a = random.randint(5, 21)
-                    # 绘制边界框
-                    draw_a.rectangle([(start_col, start_row), (end_col, end_row)], outline=color, width=wid_a)
-
-                    # 标记编号
-                    block_number = f"Block-bounding {r + 1}{c + 1}"
-                    draw_a.text((start_col + 10, start_row + 10), block_number, font=font, fill=color)
-
-                    # 画块的边框和编号
-                    if plot:
-                        # 遍历每个图像块并在画布上进行拼接、编号和边界框标记
-                        for i, block in enumerate(image_blocks):
-                            # 生成随机颜色
-                            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-                            # 计算当前图像块在画布上的位置
-                            row = i // (num_cols + 1)
-                            col = i % (num_cols + 1)
-                            start_row = row * stride_height
-                            end_row = start_row + self.crop_height
-                            start_col = col * stride_width
-                            end_col = start_col + self.crop_width
-
-                            # block
-                            block = image_block.block
-
-                            # 在画布上拼接图像块
-                            boxuu = (start_col, start_row, end_col, end_row)
-                            canvas.paste(block, (start_col, start_col))
-                            # canvas.paste(block, boxuu)
-
-                            # # # 绘制边界框 ---这里要用到end_row,end_col
-                            # draw_b.rectangle([(start_col, start_row), (end_col, end_row)], outline=color, width=6)
-
-                            # 标记编号
-                            block_number = f"Block {row + 1}{col + 1}"
-                            draw_b.text((start_col + 10, start_row + 10), block_number, font=font, fill=color)
-
-                            if show_onebyone:
-                                # 显示拼接后的图像
-                                # 新建画布，然后一块一块往画布上写入，后写入的会覆盖之前的
-                                canvas.show()
-
-
-            # # 显示拼接后的图像
-            if plot and all_box_show:
-                # 全部的overlap框
-                # print("a")
-                draw_img.show()
-            elif plot and coverred_box_show:
-                # print("b")
-                # 有覆盖的框
-                canvas.show()
-            else:
-                pass
-
+            # # # # 有覆盖的框, crop依次写入之后
+            # canvas.show()
+            
             return image_blocks, padded_image
 
-    # slide window for semantic segmentation  # input: PIL.Image.Image
-    def paddedImageCrop_seg(self, image, crop_size=(640,640), overlap=(0, 0)):
+    def paddedImageCrop(self, image, crop_size=(640,640), overlap=(0, 0)):
             width, height = image.size
             self.crop_width, self.crop_height = crop_size
 
@@ -320,9 +278,10 @@ class pil_img:
 
             return image_blocks, padded_image
        
-
-# 推理的时候把一张图片划分为了多个block， 然后把这一堆block<'class' ImageBlock>作为一个batch传到model，用gpu并行来加速
-# 肯定输出的也是一个class  OutBlock
-# 
-
-
+       
+if __name__ == '__main__':
+    img_path = r"D:\chyCodespace\repo\origin\CV_PROJECTION\U2NET_SS\raw_data\rgbpngs\train\image(17).png"
+    pil_img = pil_img()
+    image = Image.open(img_path)
+    pil_img.paddedImageCrop_new(image, crop_size=(640,640), overlap=(0, 0), draw_cropRectangle=True)
+    # img_blocks = pil_img.paddedImageCrop(image, crop_size=(640,640), overlap=(200, 200))
