40cc42452ffb86a6d855d36594cdb1fda95900b8
fhfeishi <fhfeishi@gmail.com>
2024-05-09 22:42:35 +0800

    commit utils

---
+++ CV_PROJECTION/SS_u2net/utils/my_transforms.py
@@ -0,0 +1,48 @@
+import random 
+import numpy as np
+import torch
+import PIL.Image as Image
+
+
+#---transforms for check input_data--also use in infer---
+# ensure input image mode: RGB
+def cvtColor(image):
+    if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
+        # h, w, 3
+        return image
+    else:
+        image = image.convert('RGB')
+        return image
+    
+# normalization
+def normalization(image):
+    image /= 255.0
+    return image
+
+
+
+# ---transforms for model.train()---------------------
+def train_transforms():
+    pass
+
+
+
+
+
+
+
+# ---transforms for model.eval()----------------------
+def eval_transforms():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CV_PROJECTION/SS_u2net/utils/train_utils.py
@@ -0,0 +1,21 @@
+
+# 
+
+# def evaluate
+
+# def criterion
+
+# def fit_one_epoch
+
+
+
+
+
+
+
+
+
+
+
+
+
