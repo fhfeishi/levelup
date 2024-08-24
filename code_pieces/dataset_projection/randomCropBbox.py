104f1e067e84ef15b0f84a39946c1e6c4f084898
fhfeishi <fhfeishi@gmail.com>
2024-08-24 15:29:30 +0800

    update randomCropBbox.py

--- code_pieces/dataset_projection/randomCropBbox.py
+++ code_pieces/dataset_projection/randomCropBbox.py
@@ -1,6 +1,6 @@
 import random
 
-def randomCropBbox(opencvImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7, max_attempts=100):
+def randomCropBbox_raw(opencvImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7, max_attempts=100):
     h,w,_ = opencvImage.shape
     cw,ch = crop_size
     assert w>cw and h>ch, "crop < image !!!"
@@ -17,13 +17,13 @@ def randomCropBbox(opencvImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7,
             cropBbox_y1 = max(y1, bbox[1])
             cropBbox_x2 = min(x2, bbox[2])
             cropBbox_y2 = min(y2, bbox[3])
-            cropBbox_area = (cropBbox_x2-cropBbox_x1)*(cropBbox_y2-cropBbox_y1)
-            bbox_area = (bbox[2]-bbox[0])*(bbox[3]-bbox[1])
+            cropBbox_area = max(0,(cropBbox_x2-cropBbox_x1)) * max(0,(cropBbox_y2-cropBbox_y1))
+            bbox_area = (bbox[2]-bbox[0]) * (bbox[3]-bbox[1])
             # bbox_area  > crop_area
             if bbox_area > cw*ch:
                 # large obj. 
                 if cropBbox_area >= bbox_area*cropBbox_ratio:
-                    new_bboxes.append([cropBbox_x1-x1, cropBbox_y1-y1, cropBbox_x2-x1, cropBbox_y2-y2])
+                    new_bboxes.append([cropBbox_x1-x1, cropBbox_y1-y1, cropBbox_x2-x2, cropBbox_y2-y2])
                     # continue
                 else:
                     # obj is too large
@@ -31,7 +31,7 @@ def randomCropBbox(opencvImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7,
             else:
                 # bbox_area  =< crop_area
                 if cropBbox_area >= bbox_area*cropBbox_ratio:
-                    new_bboxes.append([cropBbox_x1-x1, cropBbox_y1-y1, cropBbox_x2-x1, cropBbox_y2-y2])
+                    new_bboxes.append([cropBbox_x1-x1, cropBbox_y1-y1, cropBbox_x2-x2, cropBbox_y2-y2])
                 else:
                     # crop_bbox too small
                     continue
@@ -67,10 +67,6 @@ def randomCropBboxA(opencvImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7
             
             if cropBbox_area >= bbox_area*cropBbox_ratio:
                     new_bboxes.append([bbox_x1-x1, bbox_y1-y1, bbox_x2-x1, bbox_y2-y2])
-                    # continue
-            else:
-                # obj is too large
-                continue
             
             if len(new_bboxes) > 0:
                 cropped_image = opencvImage[y1:y2, x1:x2]
@@ -98,21 +94,9 @@ def randomCropBboxB(pillowImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7
             cropBbox_area = max(0, bbox_x2-bbox_x1) * max(0, bbox_y2-bbox_y1)
             bbox_area = (bbox[2]-bbox[0]) * (bbox[3]-bbox[1])
             
-            # bbox_area  > crop_area
-            if bbox_area > cw*ch:
-                # large obj.  if obj is too large?
-                if cropBbox_area >= bbox_area*cropBbox_ratio:
-                    new_bboxes.append([bbox_x1-x1, bbox_y1-y1, bbox_x2-x1, bbox_y2-y2])
-                    # continue
-                else:
-                    continue
-            else:
-                # bbox_area  =< crop_area
-                if cropBbox_area >= bbox_area*cropBbox_ratio:
-                    new_bboxes.append([bbox_x1-x1, bbox_y1-y1, bbox_x2-x1, bbox_y2-y2])
-                else:
-                    # crop_bbox too small
-                    continue
+            if cropBbox_area >= bbox_area*cropBbox_ratio:
+                new_bboxes.append([bbox_x1-x1, bbox_y1-y1, bbox_x2-x1, bbox_y2-y2])
+
             if len(new_bboxes) > 0:
                 cropped_image = pillowImage.crop((x1,y1,x2,y2)) # Image.crop((left,top,right,bottom)) 
                 return cropped_image, new_bboxes
