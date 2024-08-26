38a899f2e8ed3306641d3d7ebc063e75e023b8d2
feishi <fhfeishi@gmail.com>
2024-08-26 11:39:37 +0800

    update randomCropBbox code

--- code_pieces/dataset_projection/randomCropBbox.py
+++ code_pieces/dataset_projection/randomCropBbox.py
@@ -31,7 +31,7 @@ def randomCropBbox_raw(opencvImage, bboxes, crop_size=(640,640), cropBbox_ratio=
             else:
                 # bbox_area  =< crop_area
                 if cropBbox_area >= bbox_area*cropBbox_ratio:
-                    new_bboxes.append([cropBbox_x1-x1, cropBbox_y1-y1, cropBbox_x2-x2, cropBbox_y2-y2])
+                    new_bboxes.append([cropBbox_x1-x1, cropBbox_y1-y1, cropBbox_x2-x1, cropBbox_y2-y1])
                 else:
                     # crop_bbox too small
                     continue
@@ -66,7 +66,7 @@ def randomCropBboxA(opencvImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7
             bbox_area = (bbox[2]-bbox[0]) * (bbox[3]-bbox[1])
             
             if cropBbox_area >= bbox_area*cropBbox_ratio:
-                    new_bboxes.append([bbox_x1-x1, bbox_y1-y1, bbox_x2-x1, bbox_y2-y2])
+                    new_bboxes.append([bbox_x1-x1, bbox_y1-y1, bbox_x2-x1, bbox_y2-y1])
             
             if len(new_bboxes) > 0:
                 cropped_image = opencvImage[y1:y2, x1:x2]
@@ -95,7 +95,7 @@ def randomCropBboxB(pillowImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7
             bbox_area = (bbox[2]-bbox[0]) * (bbox[3]-bbox[1])
             
             if cropBbox_area >= bbox_area*cropBbox_ratio:
-                new_bboxes.append([bbox_x1-x1, bbox_y1-y1, bbox_x2-x1, bbox_y2-y2])
+                new_bboxes.append([bbox_x1-x1, bbox_y1-y1, bbox_x2-x1, bbox_y2-y1])
 
             if len(new_bboxes) > 0:
                 cropped_image = pillowImage.crop((x1,y1,x2,y2)) # Image.crop((left,top,right,bottom)) 
