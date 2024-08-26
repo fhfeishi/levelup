91dcbdd72eea1ab776e627a8d30b699fb12a4d73
fhfeishi <fhfeishi@gmail.com>
2024-08-26 22:16:32 +0800

    update randomcropbbox

--- code_pieces/dataset_projection/randomCropBbox.py
+++ code_pieces/dataset_projection/randomCropBbox.py
@@ -23,7 +23,7 @@ def randomCropBbox_raw(opencvImage, bboxes, crop_size=(640,640), cropBbox_ratio=
             interAreaRatio = cropBbox_area / bbox_area
             interAreaRatios[tuple(bbox)] = interAreaRatio
             
-            interAreaRatios = dict(sorted(interAreaRatios.items(), key=lambda item: item[1], reverse=True))
+            # interAreaRatios = dict(sorted(interAreaRatios.items(), key=lambda item: item[1], reverse=True))
 
             if any(ratio >= cropBbox_ratio for ratio in interAreaRatios.values()):
                 if all(ratio >= cropBbox_ratio or ratio == 0 for ratio in interAreaRatios.values()):
@@ -67,7 +67,7 @@ def randomCropBboxA(opencvImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7
             interAreaRatio = cropBbox_area / bbox_area
             interAreaRatios[tuple(bbox)] = interAreaRatio
             
-            interAreaRatios = dict(sorted(interAreaRatios.items(), key=lambda item: item[1], reverse=True))
+            # interAreaRatios = dict(sorted(interAreaRatios.items(), key=lambda item: item[1], reverse=True))
 
             if any(ratio >= cropBbox_ratio for ratio in interAreaRatios.values()):
                 if all(ratio >= cropBbox_ratio or ratio == 0 for ratio in interAreaRatios.values()):
@@ -108,7 +108,7 @@ def randomCropBboxB(pillowImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7
             interAreaRatio = cropBbox_area / bbox_area
             interAreaRatios[tuple(bbox)] = interAreaRatio
             
-            interAreaRatios = dict(sorted(interAreaRatios.items(), key=lambda item: item[1], reverse=True))
+            # interAreaRatios = dict(sorted(interAreaRatios.items(), key=lambda item: item[1], reverse=True))
 
             if any(ratio >= cropBbox_ratio for ratio in interAreaRatios.values()):
                 if all(ratio >= cropBbox_ratio or ratio == 0 for ratio in interAreaRatios.values()):
@@ -129,3 +129,54 @@ def randomCropBboxB(pillowImage, bboxes, crop_size=(640,640), cropBbox_ratio=0.7
     return pillowImage, bboxes
     
 
+"""
+h,w,_ = opencvImage.shape
+imageCrop = opencvImage[y1:y2, x1:x2]
+
+w,h = pillowImage.size
+imageCrop = pillowImage.crop((x1,y1,x2,y2))
+
+"""
+
+
+def myCrop(pillowImage, bboxes, crop_size=(640,640), ratio=0.7, maxAttempts=100):
+    w,h = pillowImage.size
+    
+    for _ in range(maxAttempts):
+        x1 = random.randint(0, (w-crop_size[0]+1))
+        y1 = random.randint(0, (h-crop_size[1]+1))
+        x2 = x1 + crop_size[0]
+        y2 = y1 = crop_size[1]
+        
+        interAreaDicts = {}
+        for bbox in bboxes:
+            interBbox_x1 = max(x1, bbox[0])
+            interBbox_y1 = max(x1, bbox[1])
+            interBbox_x2 = max(x1, bbox[2])
+            interBbox_y2 = max(x1, bbox[3])
+            
+            interArea = (interBbox_x2-interBbox_x1) * (interBbox_y2-interBbox_y1)
+            bboxArea = (bbox[3]-bbox[1]) * (bbox[2]-bbox[1])
+            interAreaDicts[tuple(bbox)] = interArea / bboxArea
+        
+        # interAreaDicts = dict(sorted(interAreaDicts.items, key=lambda item:item[1], reverse=True))
+        
+        if any(ratio >= ratio for ratio in interAreaDicts.values()) and \
+        all(ratio >= ratio or ratio == 0 for ratio in interAreaDicts.values()):
+            
+            croppedImage = pillowImage.crop((x1, y1, x2, y2))
+            
+            new_bboxes = []
+            for key, value in interAreaDicts:
+                if value >= ratio:
+                    new_bbox = {
+                        max(0,key[0]-x1),
+                        max(0,key[1]-y1),
+                        min(crop_size[0],key[2]-x2),
+                        min(crop_size[1],key[3]-y2),
+                    }
+                    new_bboxes.append(new_bbox)
+            
+            return croppedImage, new_bboxes
+        
+    return pillowImage, bboxes
